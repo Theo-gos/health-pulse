58327206b48037a505f326dee01b8a57c17ce6ad
Theo Le <theo.le.goldenowl@gmail.com>
2024-04-11 10:51:16 +0700

    Updated build

--- public/build/assets/Logo-lwkzWSUZ.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-3mzWtfnt.js";const t="http://18.142.226.214/build/assets/logo-B4anD0Yd.svg";function n({width:o="50px"}){return s.jsx("img",{src:t,width:o})}export{n as default};
